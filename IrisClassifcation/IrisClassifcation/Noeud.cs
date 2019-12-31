cdcca58bc2096b433c22b7f9e8755c75d9835c6e
Sébastien M.L <sebastien.lejeune6@gmail.com>
2019-12-31 17:32:53 +0100

    add 3 classes for the Tree (Arbre, noeud, sommet)

---
+++ IrisClassifcation/IrisClassifcation/Noeud.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IrisClassifcation
+{
+    class Noeud<T>
+    {
+        private T valeur;
+        private Noeud<T> filsG;
+        private Noeud<T> filsD;
+
+        public Noeud(T valeur, Noeud<T> filsG, Noeud<T> filsD)
+        {
+            this.valeur = valeur;
+            this.filsG = filsG;
+            this.filsD = filsD;
+        }
+        public T Valeur
+        {
+            get { return valeur; }
+        }
+        public Noeud<T> FilsG
+        {
+            get { return filsG; }
+            set { filsG = value; }
+        }
+        public Noeud<T> FilsD
+        {
+            get { return filsD; }
+            set { filsD = value; }
+        }
+    }
+}
---
+++ IrisClassifcation/IrisClassifcation/Sommet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IrisClassifcation
+{
+    class Sommet<T>
+    {
+        private T valeur;
+        private Sommet<T> filsG;
+        private Sommet<T> filsD;
+        private Sommet<T> parent;
+
+        public Sommet(Sommet<T> parent, T valeur, Sommet<T> filsG, Sommet<T> filsD)
+        {
+            this.parent = parent;
+            this.valeur = valeur;
+            this.filsG = filsG;
+            this.filsD = filsD;
+        }
+        public T Valeur
+        {
+            get { return valeur; }
+        }
+        public Sommet<T> Parent
+        {
+            get { return parent; }
+        }
+        public Sommet<T> FilsG
+        {
+            get { return filsG; }
+            set { filsG = value; }
+        }
+        public Sommet<T> FilsD
+        {
+            get { return filsD; }
+            set { filsD = value; }
+        }
+    }
+}
