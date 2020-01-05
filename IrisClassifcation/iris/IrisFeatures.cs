dea3eca1225637dc4b0da0cbd0b04176c638df9d
gazorby <gazorby@pm.me>
2020-01-05 20:18:27 +0100

    refactor(main): add iris asking

---
+++ IrisClassifcation/iris/IrisFeatures.cs
@@ -0,0 +1,11 @@
+namespace iris
+{
+    public enum IrisFeatures
+    {
+        Type = 0,
+        SepalLenght = 1,
+        SepalWidth = 2,
+        PetalLength = 3,
+        PetalWidth = 4
+    }
+}
