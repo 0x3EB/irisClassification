706c1890617468dfe3be3a47432bf03941bbc870
gazorby <mmacnab22@gmail.com>
2020-01-11 11:15:06 +0100

    refactor(Model): add a working split array method

--- IrisClassifcation/iris/Program.cs
+++ IrisClassifcation/iris/Program.cs
@@ -16,14 +16,11 @@ namespace iris
         private static void Main(string[] args)
         {
             // AskParameters();
-            // var choice = CheckInt(min: 1, max: 3, err: "Wrong choice", hook: () => Console.WriteLine(MenuString));
+            //var choice = Model.CheckInt(err: "Wrong choice", min: 1, max: 3, message: MenuString) ;
             // AskTestIris();
             var file = Path.Combine(Directory.GetCurrentDirectory(), "iris.txt");
             var model = new Model(file);
-            foreach(var a in model.ResizedTree(1))
-            {
-                Console.WriteLine(a);
-            }
+            model.GetTree().HierarchyPrint(model.GetTree().Root, 2);
             // var r = new readFile(file);
             // Console.WriteLine("test "+r.GetFile(121,5)[2,0]);
             // Tree<double> ab = new Tree<double>();
@@ -31,7 +28,7 @@ namespace iris
             // Node<double> n1 = new Node<double>(1, null, null);
             // Console.WriteLine(ab.Height(n1));
             // Console.WriteLine(IsSampleDiv(ab, _maxTreeSize, n1, _minIndividuals, r.getNbLine(), _maxAccuracy, _minAccuracy, _irisType, r));
-             Console.ReadLine();
+             // Console.ReadLine();
         }
     }
 }
