5c2a5582d0798093f0228570a300a540e6a841cf
Sébastien M.L <sebastien.lejeune6@gmail.com>
2020-01-12 20:42:04 +0100

    set the loop for the menu

--- IrisClassifcation/iris/Program.cs
+++ IrisClassifcation/iris/Program.cs
@@ -12,9 +12,10 @@ namespace iris
     {
         private const string MenuString = "(1) : Display tree height\n" +
                                           "(2) : Display tree width\n" +
-                                          "(3) : Display tree\n" + 
-                                          "(4) : Display leafs\n" + 
-                                          "(5) : Predict";
+                                          "(3) : Display tree\n" +
+                                          "(4) : Display leafs\n" +
+                                          "(5) : Predict\n" +
+                                          "(0) : Exist";
 
         private static void Main(string[] args)
         {
@@ -22,28 +23,37 @@ namespace iris
             var model = new Model(file, 1);
             model.AskParameters(true);
             //model.AskParameters();
-            var choice = Model.CheckInt(err: "Wrong choice", min: 1, max: 5, message: MenuString) ;
             model.Build();
-            switch (choice)
+            bool exit = false;
+            do
             {
-                case 1:
-                    model.DisplayTreeHeight();
-                    break;
-                case 2:
-                    model.DisplayTreeWidth();
-                    break;
-                case 3:
-                    model.DisplayTree(1);
-                    break;
-                case 4:
-                    model.DisplayLeafs();
-                    break;
-                case 5:
-                    model.DisplayPredict(Model.AskTestIris());
-                    break;
-                default:
-                    break;;
-            }
+                var choice = Model.CheckInt(err: "Wrong choice", min: 0, max: 5, message: MenuString);
+                switch (choice)
+                {
+                    case 0:
+                        exit = true;
+                        break;
+                    case 1:
+                        model.DisplayTreeHeight();
+                        break;
+                    case 2:
+                        model.DisplayTreeWidth();
+                        break;
+                    case 3:
+                        model.DisplayTree(1);
+                        break;
+                    case 4:
+                        model.DisplayLeafs();
+                        break;
+                    case 5:
+                        model.DisplayPredict(Model.AskTestIris());
+                        break;
+                    default:
+                        exit = true;
+                        break;
+                        ;
+                }
+            } while (!exit);
         }
     }
 }
