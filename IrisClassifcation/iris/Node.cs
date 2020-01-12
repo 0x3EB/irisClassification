86d8e13278881afa8e2740703939b1ab8aed38f1
Sébastien M.L <sebastien.lejeune6@gmail.com>
2020-01-12 23:07:17 +0100

    add comments in Model.cs

--- IrisClassifcation/iris/Node.cs
+++ IrisClassifcation/iris/Node.cs
@@ -14,12 +14,24 @@ namespace iris
             LChild = lChild;
             RChild = rChild;
         }
+        /// <summary>
+        /// Getter for the value of the node
+        /// </summary>
         public T[,] Array { get; }
-
+        
+        /// <summary>
+        /// Number of the column used for the best division
+        /// </summary>
         public int DivisionVar { get; set; }
 
+        /// <summary>
+        /// Getter and Setter for the Left Child of the Node
+        /// </summary>
         public Node<T> LChild { get; set; }
 
+        /// <summary>
+        /// Getter and Setter for the Right Child of the Node
+        /// </summary>
         public Node<T> RChild { get; set; }
     }
 }
