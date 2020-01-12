75db2b40249144ba349155edc03e0ba409fff228
gazorby <gazorby@pm.me>
2020-01-12 23:53:20 +0100

    refactor(Entry point): remove testing code

--- IrisClassifcation/iris/Program.cs
+++ IrisClassifcation/iris/Program.cs
@@ -19,26 +19,6 @@ namespace iris
             //model.AskParameters();
             model.Build();
             var exit = false;
-            // double[] iris1 = {7.7, 3.0, 6.1, 2.3};
-            // model.DisplayPredict(iris1);
-            // double[] iris2 = {6.2, 2.8, 4.8, 1.8};
-            // model.DisplayPredict(iris2);
-            // double[] iris3 = {5.5, 2.5, 4.0, 1.3};
-            // model.DisplayPredict(iris3);
-            // double[] iris4 = {6.7, 3.3, 5.7, 2.5};
-            // model.DisplayPredict(iris4);
-            // double[] iris5 = {6.0, 2.2, 5.0, 1.5};
-            // model.DisplayPredict(iris5);
-            // double[] iris6 = {6.0, 2.7, 5.1, 1.6};
-            // model.DisplayPredict(iris6);
-            // double[] iris7 = {5.7, 2.6, 3.5, 1.0};
-            // model.DisplayPredict(iris7);
-            // double[] iris8 = {5.8, 2.6, 4.0, 1.2};
-            // model.DisplayPredict(iris8);
-            // double[] iris9 = {5.1, 3.4, 1.5, 0.2};
-            // model.DisplayPredict(iris9);
-            // double[] iris10 = {5.4, 3.9, 1.3, 0.4};
-            // model.DisplayPredict(iris10);
             do
             {
                 var choice = Model.CheckInt("Wrong choice", 0, 5, message: MenuString);
