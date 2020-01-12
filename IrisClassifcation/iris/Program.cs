73e3b09c0303173653284016edd805e89889c485
Sébastien M.L <sebastien.lejeune6@gmail.com>
2020-01-12 19:53:48 +0100

    Display the menu and add the method for the path and the accuracy of leafs

--- IrisClassifcation/iris/Program.cs
+++ IrisClassifcation/iris/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -11,41 +12,38 @@ namespace iris
     {
         private const string MenuString = "(1) : Display tree height\n" +
                                           "(2) : Display tree width\n" +
-                                          "(3) : Display tree";
+                                          "(3) : Display tree\n" + 
+                                          "(4) : Display leafs\n" + 
+                                          "(5) : Predict";
 
         private static void Main(string[] args)
         {
-            // AskParameters();
-            //var choice = Model.CheckInt(err: "Wrong choice", min: 1, max: 3, message: MenuString) ;
-            // AskTestIris();
             var file = Path.Combine(Directory.GetCurrentDirectory(), "iris.txt");
-            // model.AskParameters(true);
-            double[] newIris = {4.9, 3.0, 1.4, 0.2};
-            var model1 = new Model(file, 1);
-            model1.Build();
-            Console.WriteLine("prediction for type 1 : " + model1.Predict(newIris));
-             Console.WriteLine("*******************");
-             model1.DisplayTree(2);
-             var model2 = new Model(file, 2);
-             model2.Build();
-             
-             Console.WriteLine("prediction for type 2 : " + model2.Predict(newIris));
-             Console.WriteLine("*******************");
-             model2.DisplayTree(2);
-             var model3 = new Model(file, 3);
-             model3.Build();
-             Console.WriteLine("prediction for type 3 : " + model3.Predict(newIris));
-             model3.DisplayTree(2);
-            // var newIris = Model.AskTestIris();
-            // model.GetTree().HierarchyPrint(model.GetTree().Root, 2);
-            // var r = new readFile(file);
-            // Console.WriteLine("test "+r.GetFile(121,5)[2,0]);
-            // Tree<double> ab = new Tree<double>();
-            //
-            // Node<double> n1 = new Node<double>(1, null, null);
-            // Console.WriteLine(ab.Height(n1));
-            // Console.WriteLine(IsSampleDiv(ab, _maxTreeSize, n1, _minIndividuals, r.getNbLine(), _maxAccuracy, _minAccuracy, _irisType, r));
-            // Console.ReadLine();
+            var model = new Model(file, 1);
+            model.AskParameters(true);
+            //model.AskParameters();
+            var choice = Model.CheckInt(err: "Wrong choice", min: 1, max: 5, message: MenuString) ;
+            model.Build();
+            switch (choice)
+            {
+                case 1:
+                    model.DisplayTreeHeight();
+                    break;
+                case 2:
+                    model.DisplayTreeWidth();
+                    break;
+                case 3:
+                    model.DisplayTree(1);
+                    break;
+                case 4:
+                    model.DisplayLeafs();
+                    break;
+                case 5:
+                    model.DisplayPredict(Model.AskTestIris());
+                    break;
+                default:
+                    break;;
+            }
         }
     }
 }
