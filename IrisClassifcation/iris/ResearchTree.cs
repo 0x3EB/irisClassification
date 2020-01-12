f59b779e62c140fa6ca9e36dd2cc78717310e6fa
gazorby <gazorby@pm.me>
2020-01-12 23:18:53 +0100

    refactor: code cleanup

--- IrisClassifcation/iris/ResearchTree.cs
+++ IrisClassifcation/iris/ResearchTree.cs
@@ -1,70 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace iris
 {
-    class ResearchTree<T>
+    internal class ResearchTree<T>
     {
-        private Top<T> _root;
-
         public ResearchTree(Top<T> root)
         {
-            this._root = root;
+            Root = root;
         }
+
         public ResearchTree()
         {
-            this._root = null;
+            Root = null;
         }
-        public Top<T> Root
-        {
-            get { return _root; }
-            set { _root = value; }
-        }
+
+        public Top<T> Root { get; set; }
+
         public Top<T> CreationSommet(T val)
         {
             return new Top<T>(null, val, null, null);
         }
+
         public bool AssocierFilsG(Top<T> parent, Top<T> child)
         {
-            bool association = false;
+            var association = false;
             if (parent != null && child != null)
-            {
                 if (parent.LChild == null)
                 {
                     parent.LChild = child;
                     association = true;
                 }
-            }
+
             return association;
         }
+
         public bool AddRChild(Top<T> parent, Top<T> child)
         {
-            bool association = false;
+            var association = false;
             if (parent != null && child != null)
-            {
                 if (parent.RChild == null)
                 {
                     parent.RChild = child;
                     association = true;
                 }
-            }
+
             return association;
         }
+
         public bool IsLeafTop(Top<T> top)
         {
-            bool leaf = false;
+            var leaf = false;
             if (top != null)
-            {
                 if (top.RChild == null && top.LChild == null)
-                {
                     leaf = true;
-                }
-            }
             return leaf;
         }
+
         public void PrefixPrint(Top<T> top)
         {
             if (top != null)
@@ -74,6 +65,7 @@ namespace iris
                 PrefixPrint(top.RChild);
             }
         }
+
         public void InfixPrint(Top<T> top)
         {
             if (top != null)
@@ -83,6 +75,7 @@ namespace iris
                 InfixPrint(top.RChild);
             }
         }
+
         public void PostfixPrint(Top<T> top)
         {
             if (top != null)
@@ -92,23 +85,16 @@ namespace iris
                 Console.Write(top.Value + " ");
             }
         }
+
         public void HierarchyPrint(Top<T> top, int gap)
         {
-
-            for (int i = 0; i < gap; i++)
-            {
-                Console.Write(" ");
-            }
+            for (var i = 0; i < gap; i++) Console.Write(" ");
             if (top != null)
             {
                 if (gap != 0)
-                {
                     Console.WriteLine("|-" + top.Value);
-                }
                 else
-                {
                     Console.WriteLine(top.Value);
-                }
                 if (!IsLeafTop(top))
                 {
                     gap++;
@@ -121,88 +107,61 @@ namespace iris
                 Console.WriteLine("|-X");
             }
         }
+
         public int NbChildren(Top<T> parent)
         {
-            int nb = 0;
+            var nb = 0;
             if (parent != null)
             {
-                if (parent.LChild != null)
-                {
-                    nb++;
-                }
-                if (parent.RChild != null)
-                {
-                    nb++;
-                }
+                if (parent.LChild != null) nb++;
+                if (parent.RChild != null) nb++;
             }
+
             return nb;
         }
+
         public int NbDescendant(Top<T> parent)
         {
             if (parent != null)
-            {
                 return NbChildren(parent) + NbDescendant(parent.RChild) + NbDescendant(parent.LChild);
-            }
-            else
-            {
-                return 0;
-            }
+            return 0;
         }
+
         public int NbFeuille(Top<T> parent)
         {
-            if (IsLeafTop(parent))
-            {
-                return 1;
-            }
+            if (IsLeafTop(parent)) return 1;
             if (parent != null)
-            {
                 return NbFeuille(parent.RChild) + NbFeuille(parent.LChild);
-            }
-            else
-            {
-                return 0;
-            }
+            return 0;
         }
+
         public int Max(int a, int b)
         {
-            int max = a;
-            if (a < b)
-            {
-                max = b;
-            }
+            var max = a;
+            if (a < b) max = b;
             return max;
         }
+
         public int Hauteur(Top<T> top)
         {
-            if (IsLeafTop(top))
-            {
-                return 1;
-            }
+            if (IsLeafTop(top)) return 1;
             if (top != null)
-            {
                 return 1 + Max(Hauteur(top.LChild), Hauteur(top.RChild));
-            }
-            else
-            {
-                return 0;
-            }
+            return 0;
         }
+
         public void InsertionSommet(Top<int> top, int val)
         {
             if (top == null)
             {
-                top = new Top<int>(null, val,null,null);
+                top = new Top<int>(null, val, null, null);
             }
             else
             {
                 if (val > top.Value)
-                {
                     InsertionSommet(top.RChild, val);
-                }
                 else
-                {
                     InsertionSommet(top.LChild, val);
-                }
             }
         }
     }
--- IrisClassifcation/iris/Top.cs
+++ IrisClassifcation/iris/Top.cs
@@ -1,38 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace iris
+﻿namespace iris
 {
-    class Top<T>
+    internal class Top<T>
     {
-        private T value;
         private Top<T> _lChild;
         private Top<T> _rChild;
-        private Top<T> _parent;
 
         public Top(Top<T> parent, T value, Top<T> lChild, Top<T> rChild)
         {
-            this._parent = parent;
-            this.value = value;
-            this._lChild = lChild;
-            this._rChild = rChild;
+            Parent = parent;
+            Value = value;
+            _lChild = lChild;
+            _rChild = rChild;
         }
-        public T Value
-        {
-            get { return value; }
-        }
-        public Top<T> Parent
-        {
-            get { return _parent; }
-        }
+
+        public T Value { get; }
+
+        public Top<T> Parent { get; }
+
         public Top<T> LChild
         {
             get { return LChild; }
             set { LChild = value; }
         }
+
         public Top<T> RChild
         {
             get { return RChild; }
